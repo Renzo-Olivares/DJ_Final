12237096102fe8f963c112ca7c61e839a236efb9
Renzo <rmolivares@renzo-olivares.dev>
2021-03-15 17:01:48 -0700

    refactor

--- dj.py
+++ dj.py
@@ -5,14 +5,10 @@ def dj(G, w, s):
     init_single_source(G,s)
     S = []
     Q = []
-    dupeChecker = set()
-
-    for vertex in G.V.values():
-        for node in vertex:
-            if node.value not in dupeChecker:
-                dupeChecker.add(node.value)
-                heapq.heappush(Q, (node.distance, node))
     
+    for vertex in G.V:
+        heapq.heappush(Q, (vertex.distance, vertex))
+
     for node in Q:
         print(node[1].value)
     
@@ -22,31 +18,40 @@ def dj(G, w, s):
         u = heapq.heappop(Q)[1]
         S.append(u)
 
-        for vertex in G.V[u.value]: #G.adj[u]
-            originalDistance = vertex.distance
-            relax(u, vertex, w, G)
-            if vertex.distance != originalDistance:
+        for vertex in G.adj[u.value]: #G.adj[u]
+            currentVertex = vertex[0]
+            originalDistance = currentVertex.distance
+            relax(u, currentVertex, w, G)
+            if currentVertex.distance != originalDistance:
                 for index, vTuple in enumerate(Q):
-                    if vTuple[1].value == vertex.value:
-                        Q[index] = (vertex.distance, vertex)
+                    if vTuple[1].value == currentVertex.value:
+                        Q[index] = (currentVertex.distance, currentVertex)
                         heapq.heapify(Q)
 
 
     for node in S:
-        print(node.value)
+        print('node:' + node.value)
+        print('node distance:' + str(node.distance))
         if node.predecessor is not None:
-            print('whut')
-            print(node.predecessor.value)
+            print('node parent:' + node.predecessor.value)
         print('\n')
                 
 def init_single_source(G, s):
-    for vertex in G.V.values():
+    for vertex in G.V:
+        if vertex.value == s:
+            vertex.distance = 0
+        else:
+            vertex.distance = float('inf')
+        vertex.predecessor = None
+        
+    for vertex in G.adj.values():
         for node in vertex:
-            if node.value == s:
-                node.distance = 0
+            currentNode = node[0]
+            if currentNode.value == s:
+                currentNode.distance = 0
             else:
-                node.distance = float('inf')
-            node.predecessor = None
+                currentNode.distance = float('inf')
+            currentNode.predecessor = None
 
 def relax(u,v,w, G):
     if v.distance > u.distance + w(u,v,G):
@@ -54,36 +59,38 @@ def relax(u,v,w, G):
         v.predecessor = u
 
 def weight(u,v,G):
-    for vertex in G.V[u.value]:
-        if vertex.value == v.value:
-            return vertex.weight
+    for vertex in G.adj[u.value]:
+        currentVertex = vertex[0]
+        if currentVertex.value == v.value:
+            return vertex[1]
+    
+    return 0
 
 class Graph:
     def __init__(self):
-        self.V = {}
+        self.V = []
+        self.adj = {}
     
-    def adj(self, u):
-        #return vertices adjacent to u
-        adjacent = []
-        temp = u.next
-        adjacent.append(temp)
-        while temp:
-            temp = temp.next
-            adjacent.append(temp)
-        
-        return adjacent
+    def setAdjacent(self, adj):
+        self.adj.clear()
+        self.adj = adj
     
-    def setEntire(self, V):
+    def setVertices(self, v):
         self.V.clear()
-        self.V = V
+        self.V = v
 
+    def addEdge(self, u, v, w):
+        if u in self.adj:
+            self.adj[u].append((Vertex(v), w))
+        else:
+            self.V.append(Vertex(u))
+            self.adj[u] = [(Vertex(v), w)]      
         
 class Vertex:
-    def __init__(self, val, weight):
+    def __init__(self, val):
         self.predecessor = None
         self.distance = None
         self.value = val
-        self.weight = weight
 
     def __lt__(self, other):
         # Overload the less than operator. This function is called when there needs to be a tie break.
@@ -93,8 +100,10 @@ def main():
     graph = Graph()
     # adjList = {'s': [Vertex('t', 10), Vertex('y', 5)], 't': [Vertex('x', 1), Vertex('y', 2)], 'y': [Vertex('t', 3), Vertex('x', 9), Vertex('z', 2)], 'x': [Vertex('z', 4)], 'z': [Vertex('x', 6), Vertex('s', 7)]}
     # adjList = {'s': [Vertex('t', 3), Vertex('y', 5)], 't': [Vertex('x', 6), Vertex('y', 2)], 'y': [Vertex('t', 1), Vertex('x', 4), Vertex('z', 6)], 'x': [Vertex('z', 2)], 'z': [Vertex('x', 7), Vertex('s', 3)]}
-    adjList = {'s': [Vertex('t', 2), Vertex('y', 4)], 't': [Vertex('x', 7), Vertex('y', 1)], 'y': [Vertex('z', 3)], 'x': [Vertex('b', 1)], 'z': [Vertex('x', 2), Vertex('b', 5)], 'b': []}
-    graph.setEntire(adjList)
+    vertexList = [Vertex('s'), Vertex('t'), Vertex('y'), Vertex('x'), Vertex('z'), Vertex('b')]
+    adjList = {'s': [(Vertex('t'), 2), (Vertex('y'),4)], 't': [(Vertex('x'),7), (Vertex('y'),1)], 'y': [(Vertex('z'),3)], 'x': [(Vertex('b'),1)], 'z': [(Vertex('x'),2), (Vertex('b'),5)], 'b': []}
+    graph.setAdjacent(adjList)
+    graph.setVertices(vertexList)
     dj(graph, weight, 's')
 
 if __name__ == "__main__":
